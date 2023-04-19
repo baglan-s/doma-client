e1072f0d8d4ce1df321b62b9aa6b199492228f39
GiadaStar <giadastar@users.noreply.github.com>
2023-04-19 09:24:33 +0300

    Update TicketMutation.php

--- src/Helpers/Mutations/TicketMutation.php
+++ src/Helpers/Mutations/TicketMutation.php
@@ -8,27 +8,10 @@ class TicketMutation
     {
         return <<<GRAPHQL
             mutation createTicket(
-                $fingerprint: String!
-                $organizationId: ID!
-                $sourceId: ID!
-                $propertyId: ID!
-                $details: String
+               $data: TicketCreateInput
             ) {
-                createTicket(
-                    data: {
-                        dv: 1
-                        sender: { dv: 1, fingerprint: $fingerprint }
-                        organization: { connect: { id: $organizationId } }
-                        property: { connect: { id: $propertyId } }
-                        source: { connect: { id: $sourceId } }
-                        details: $details
-                    }
-                ) {
+                createTicket(data: $data) {
                     id
-                    details
-                    status {
-                        name type __typename
-                    }
                 }
             }
         GRAPHQL;
