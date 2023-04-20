c69c7090b5035a826e804351be4ad69e3165065e
GiadaStar <giadastar@users.noreply.github.com>
2023-04-20 10:44:28 +0300

    Update OrganizationQuery.php

--- src/Helpers/Queries/OrganizationQuery.php
+++ src/Helpers/Queries/OrganizationQuery.php
@@ -39,4 +39,37 @@ class OrganizationQuery
             }
         GRAPHQL;
     }
+    
+    public static function allOrganizationEmployeesQuery()
+    {
+        return <<<'GRAPHQL'
+            query ($where: OrganizationEmployeeWhereInput) {
+                allOrganizationEmployees (where: $where) {
+                    id
+                    name
+                    __typename
+                    user {
+                      id
+                      name
+                      __typename
+                    }
+                    organization {
+                      id
+                      name
+                      __typename
+                    }
+                    role {
+                      id
+                      name
+                      __typename
+                      canManageTickets
+                      canManageRoles
+                      canManageContacts
+                      canManageOrganization
+                      canManageProperties
+                    }
+                }
+            }
+        GRAPHQL;
+    }
 }
