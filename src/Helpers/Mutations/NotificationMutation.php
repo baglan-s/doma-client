d836f9efefeff0fc8f6acf17ba893087f3a0708e
baglan <baglansariev@mail.ru>
2023-04-25 18:45:03 +0600

    push notification

---
+++ src/Helpers/Mutations/NotificationMutation.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BaglanS\Doma\Helpers\Mutations;
+
+class NotificationMutation
+{
+    public static function sendAppPushMessage()
+    {
+        return <<<GRAPHQL
+            mutation sendAppPushMessage ($data: SendAppPushMessageInput!) {
+                sendAppPushMessage (data: $data) {
+                    id
+                    status
+                }
+            }
+        GRAPHQL;
+    }
+}
