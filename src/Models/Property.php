ddda3b85564418606cb1c5ef677f51b0051a0180
Baglan <bsariev@hiplabs.dev>
2023-08-11 11:17:56 +0600

    added b2bAppProperty methods

--- src/Models/Property.php
+++ src/Models/Property.php
@@ -4,6 +4,7 @@ namespace BaglanS\Doma\Models;
 
 use BaglanS\Doma\Models\Model;
 use BaglanS\Doma\Helpers\Queries\PropertyQuery;
+use BaglanS\Doma\Helpers\Mutations\AppMutation;
 
 class Property extends Model
 {
@@ -19,6 +20,20 @@ class Property extends Model
     public function getProperties(array $filter = [])
     {
         $response = $this->sendQuery(PropertyQuery::allPropertiesQuery(), $filter);
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+
+    public function createB2CAppProperty(array $data)
+    {
+        $response = $this->sendQuery(AppMutation::createB2CAppProperty(), $data);
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+
+    public function deleteB2CAppProperty(string $id)
+    {
+        $response = $this->sendQuery(AppMutation::deleteB2CAppProperty(), ['id' => $id]);
 
         return json_decode($response->getBody()->getContents(), true);
     }
